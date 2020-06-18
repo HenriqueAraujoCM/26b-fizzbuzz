86af8b79be1357a916f4d645d646c19cc26c2cd4
HenriqueAraujoCM <henriquecmaraujo@tecnico.ulisboa.pt>
2020-06-18 16:38:16 +0000

    created isnot luck ro print lucky when number contains a 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,26 @@
+def isnotlucky(val):
+    while val:
+        digit = val%10
+
+        if digit == 3:
+            print("lucky")
+            return False
+
+        val //= 10
+    return True
+
 def fizzbuzz(val):
-    if val%3 != 0:
-        if val%5 != 0:
-            print(val)
+    if isnotlucky(val):
+        if val%3 != 0:
+            if val%5 != 0:
+                print(val)
+            else:
+                print("buzz")
         else:
-            print("buzz")
-    else:
-        if val%5 != 0:
-            print("fizz")
-        else:
-            print("fizzbuzz")
+            if val%5 != 0:
+                print("fizz")
+            else:
+                print("fizzbuzz")
 
 for i in range(1,20):
     fizzbuzz(i)
